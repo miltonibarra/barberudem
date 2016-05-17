b67cf861a96745188470d31089f4a7717084b971
Milton Ibarra <milton.ibarra@gmail.com>
2016-05-17 00:23:23 -0500

    Final Refactory
    Final Refactory

--- src/co/com/barberudem/controller/startController.java
+++ src/co/com/barberudem/controller/startController.java
@@ -6,21 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.com.barberudem.model.FirstPageDTOold;
+import co.com.barberudem.model.FirstPageDTO;
 import co.com.barberudem.services.BarberFacade;
+
 
 @Controller
 public class startController {
-	
-	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public ModelAndView start(@ModelAttribute("imagenes") FirstPageDTOold firstPageDTO) {
-		BarberFacade barberFacade = BarberFacade.getInstance();		
-		return new ModelAndView("index", "imagenes", barberFacade.getFirstPageDTO());
-	}
 
-	 @RequestMapping(value="/lugares", method=RequestMethod.GET)
-	  public ModelAndView places(@ModelAttribute("imagenes") FirstPageDTOold firstPageDTO) {
-	    BarberFacade barberFacade = BarberFacade.getInstance();   
-	    return new ModelAndView("places", "imagenes", barberFacade.getFirstPageDTO());
-	  }
+  @RequestMapping(value = "/start", method = RequestMethod.GET)
+  public ModelAndView start(@ModelAttribute("imagenes") FirstPageDTO firstPageDTO) {
+    BarberFacade barberFacade = BarberFacade.getInstance();
+    return new ModelAndView("index", "imagenes", barberFacade.getFirstPageDTO());
+  }
+
+  @RequestMapping(value = "/lugares", method = RequestMethod.GET)
+  public ModelAndView places(@ModelAttribute("imagenes") FirstPageDTO firstPageDTO) {
+    BarberFacade barberFacade = BarberFacade.getInstance();
+    return new ModelAndView("places", "imagenes", barberFacade.getFirstPageDTO());
+  }
 }
